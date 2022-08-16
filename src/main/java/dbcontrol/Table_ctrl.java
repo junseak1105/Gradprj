bf40f98d6e8a5f85409a9cfd360363f002048556
junseak1105 <57184613+junseak1105@users.noreply.github.com>
2022-08-16 18:13:48 +0900

    테이블 생성 페이지 작성 ver.1 - 테이블명 중복확인 ajax 통신 성공 - 컬럼 추가, 삭제, 전체삭제 기능 js로 구현 완료

--- src/main/java/dbcontrol/Table_ctrl.java
+++ src/main/java/dbcontrol/Table_ctrl.java
@@ -16,6 +16,13 @@ public interface Table_ctrl extends QueryBuilder {
 
 	public String setTableInfo(ArrayList coldata, Table_Keyoption_DTO keyoption);// 테이블
 
+	/*
+	 * 테이블 생성 인자 구조
+	 * tablename: 테이블 명
+	 * tableexplain: 테이블 설명
+	 * coldata: 데이터 형태(Table_col_data(col명,type,coloption)
+	 * keyoption:데이터 형태(Table_Keyoption_DTO(pk[],uq[],fk[])
+	 */
 	public String Create_query(String tablename, String tableexplain, ArrayList coldata, Table_Keyoption_DTO keyoption);
 	
 	public String Drop_query(String tablename); //테이블 삭제, table_id 가져와서 삭제
