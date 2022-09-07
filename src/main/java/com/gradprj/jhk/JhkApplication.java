0cbbedc13c78c47dd6f18ae9374f12e9dcb7e202
junseak1105 <57184613+junseak1105@users.noreply.github.com>
2022-09-07 10:10:12 +0900

    Spring Base
    Spring 프로젝트로 이전 시작

---
+++ src/main/java/com/gradprj/jhk/JhkApplication.java
@@ -0,0 +1,13 @@
+package com.gradprj.jhk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradprj/jhk/JhkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradprj.jhk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
