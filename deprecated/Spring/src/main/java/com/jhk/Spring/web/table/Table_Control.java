32da7fad687e5b2ec38147e15955e1eeb86e7c01
김재훈 <57184613+junseak1105@users.noreply.github.com>
2022-10-21 08:11:57 +0900

    delete deprecated

--- deprecated/Spring/src/main/java/com/jhk/Spring/web/table/Table_Control.java
+++
@@ -1,7 +0,0 @@
-package com.jhk.Spring.web.table;
-
-import com.jhk.Spring.web.DBService.DB_Service;
-
-public interface Table_Control {
-    public String Execute(TableRepository table, DB_Service db);
-}
