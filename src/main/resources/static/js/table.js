2ced8dee78d3298b66e91e53c691e21b9aeb91d9
김재훈 <57184613+junseak1105@users.noreply.github.com>
2022-10-23 03:02:44 +0900

    print 상하단 커스텀 기능 제작중

--- src/main/resources/static/js/table.js
+++ src/main/resources/static/js/table.js
@@ -1,43 +1,167 @@
-/*Get information all cells*/
-
-$(document).keyup(function () {
-    var tdRows = $('#inputTd').val();
-    var trColl = $('#inputTr').val();
-    var tdRows = $('#inputTd').val();
-});
-
 $(document).ready(function () {
+    $('#addHeadBtn').click(function () {
+        $('#createstatus').val('thead');
+        $('#TableCreateModal').modal('show');
+    });
 
-    $('#btn').click(function () {
+    $('#addFootBtn').click(function () {
+        $('#createstatus').val('tfoot');
+        $('#TableCreateModal').modal('show');
+    });
 
-        $('#tablesSource').empty();
-        $('#tablesCode').empty();
+    $('#mergeBtn').click(function () {
+        mergeCells();
+    });
 
-        $('#tablesResult').show();
+    $('#deleterowBtn').click(function () {
+        deleteRow();
+    });
 
-        var trColl = $('#inputTr').val();
-        var tdRows = $('#inputTd').val();
-
-        $('#tablesSource').append('&lt;table&gt;');
-        $('#tablesCode').append('<table>');
-
-        for (a = 1; a <= tdRows; a++) {
-
-            $('#tablesSource').append('&lt;tr&gt;');
-            $('#tablesCode').append('<tr>');
-
-            for (b = 1; b <= trColl; b++) {
-
-                $('#tablesSource').append('&lt;td&gt;' + b + '\) result' + '&lt;/td&gt;');
-                $('#tablesCode').append('<td>' + b + '\) result ' + '</td>');
+    $('#createBtn').click(function () {
+        let status = $('#createstatus').val();
+        let col = $('#col').val();
+        let row = $('#row').val();
+        for (let i = 0; i < row; i++) {
+            let tr = $('<tr></tr>');
+            for (let j = 0; j < col; j++) {
+                let td = $('<td class="colresize"><input type="text" value=" "/></td>');
+                tr.append(td);
             }
+            $('#' + status).append(tr);
+        }
+        $('#TableCreateModal').modal('hide');
+    });
 
-            $('#tablesSource').append('&lt;/tr&gt;');
-            $('#tablesCode').append('</tr>');
+    $(document).on('mouseover', '.colresize', function (e) {
+        if (e.pageX - $(this).offset().left > $(this).width() - 5 ) {
+            $(this).css('cursor', 'col-resize');
+        }else if(e.pageY - $(this).offset().top > $(this).height() - 5){
+            $(this).css('cursor', 'row-resize');
+        }
+        else{
+            $(this).css('cursor', 'default');
+        }
+    });
+
+    $(document).on('mousedown', '.colresize', function (e) {
+        if (e.pageX - $(this).offset().left > $(this).width() - 5 ) {
+            $(this).addClass('resizingx');
+            $(document).mousemove(function (e) {
+                e.preventDefault();
+                let offset = $('.resizingx').offset();
+                let x = e.pageX - offset.left;
+                if (x > 10 && x < $(window).width() - 10) {
+                    $('.resizingx').width(x);
+                }else if(x < 10){
+                    $('.resizingx').width(10);
+                }
+            });
+        }
+        if (e.pageY - $(this).offset().top > $(this).height() - 5 ) {
+            $(this).parent().find('td').addClass('resizingy');
+            $(this).addClass('resizingy_selected');
+            $(document).mousemove(function (e) {
+                e.preventDefault();
+                let offset = $('.resizingy_selected').offset();
+                let y = e.pageY - offset.top;
+                if (y > 10 && y < $(window).height() - 10) {
+                    console.log(y);
+                    $('.resizingy').height(y);
+                    // $(this).parent().find('.resizingy').height(y);
+                }else if(y < 10){
+                    $('.resizingy').height(10);
+                    // $(this).parent().find('.resizingy').height(10);
+                }
+
+            });
         }
 
-        $('#tablesSource').append('&lt;/table&gt;');
-        $('#tablesCode').append('</table>');
+    });
+    //on mouseup remove the resizing class and unbind the mousemove event
+    $(document).on('mouseup', function (e) {
+        e.preventDefault();
+        $('.colresize').removeClass('resizingx');
+        $('.colresize').removeClass('resizingy');
+        $('.colresize').removeClass('resizingy_selected');
+        $('.colresize').css('cursor', 'default');
+        $(document).unbind('mousemove');
+
+        $('#result_thead').val($('#thead').html());
+        $('#result_tfoot').val($('#tfoot').html());
+    });
+
+    $(document).on('mousedown', '.colresize', function (e) {
+        if($(this).hasClass('selected')){
+            $(this).removeClass('selected');
+            $(this).css('background-color', 'white');
+        }else{
+            $(this).addClass('selected');
+            $(this).css('background-color', 'lightblue');
+        }
+    });
+
+});
+
+function deleteRow() {
+    let selected = $('.selected');
+    let tr = selected.first().parent();
+    tr.remove();
+}
+
+function mergeCells() {
+    let selected = $('.selected');
+    let first = selected.first();
+    let last = selected.last();
+    let firstRow = first.parent().index();
+    let firstCol = first.index();
+    let lastRow = last.parent().index();
+    let lastCol = last.index();
+    let rowSpan = lastRow - firstRow + 1;
+    let colSpan = lastCol - firstCol + 1;
+    let value = first.html();
+
+    let td = $('<td class="colresize" rowspan="' + rowSpan + '" colspan="' + colSpan + '"><input type="text" value="'+value+'"/></td>');
+
+    let merged = false;
+    selected.each(function () {
+        //if already merged, do nothing
+        if ($(this).attr('rowspan') || $(this).attr('colspan')) {
+            merged = true;
+            return true;
+        }
+    });
+    if (merged) {
+        unmergeCells();
+    }else{
+        first.replaceWith(td);
+        selected.each(function () {
+            if ($(this).index() != first.index() || $(this).parent().index() != first.parent().index()) {
+                $(this).remove();
+            }
+        });
+    }
+}
+
+function unmergeCells() {
+    let selected = $('.selected');
+    selected.each(function () {
+        //while unmerging cell number should be kept same
+        let rowSpan = $(this).attr('rowspan');
+        let colSpan = $(this).attr('colspan');
+        if (rowSpan || colSpan) {
+            let value = $(this).html();
+            let tr = $(this).parent();
+            let index = $(this).index();
+            $(this).remove();
+            for (let i = 0; i < rowSpan; i++) {
+                let tr = $('<tr></tr>');
+                for (let j = 0; j < colSpan; j++) {
+                    let td = $('<td class="colresize">'+value+'</td>');
+                    tr.append(td);
+                }
+                tr.insertAfter(tr);
+            }
+        }
 
     });
-});
+}
--- src/main/webapp/WEB-INF/view/tablegenerator.jsp
+++ src/main/webapp/WEB-INF/view/tablegenerator.jsp
@@ -2,7 +2,18 @@
          pageEncoding="UTF-8" %>
 <%@include file="includes/injection.jsp" %>
 <script src="/js/table.js"></script>
+<style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
 
+    table td, table th {
+        border: 1px solid #000;
+        padding: 5px;
+    }
+
+</style>
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header" id="page-header"></h1>
@@ -16,17 +27,38 @@
             <div class="panel-heading"></div>
             <!-- /.panel-heading -->
             <div class="panel-body">
-                <table id="dg" class="table table-striped table-bordered table-hover">
-                    <thead id="table-header"></thead>
+                <div>
+                    <button type="button" class="btn btn-primary" id="addHeadBtn">헤더 추가</button>
+                    <button type="button" class="btn btn-primary" id="addFootBtn">푸터 추가</button>
+                    <button type="button" class="btn btn-primary" id="mergeBtn">병합</button>
+                    <button type="button" class="btn btn-danger" id="deleterowBtn">열삭제</button>
+                </div>
+                <table id="table">
+                    <thead>
+                    <tr>
+                        <th colspan="100%">
+                            <table  id="thead" style="width: 100%;border-collapse: collapse">
+                            </table>
+                        </th>
+                    </tr>
+                    </thead>
                     <tbody>
                     <tr>
-                        내용이 들어갑니다.
+                        <td colspan="100%">내용이 들어갑니다.</td>
                     </tr>
                     </tbody>
-                    <tfoot id="table-footer"></tfoot>
+                    <tfoot>
+                    <tr>
+                        <th colspan="100%">
+                            <table  id="tfoot" style="width: 100%;border-collapse: collapse">
+                            </table>
+                        </th>
+                    </tr>
+                    </tfoot>
                 </table>
+                <input type="text" id="result_thead" style="width: 100%;height: 100px" hidden>
+                <input type="text" id="result_tfoot" style="width: 100%;height: 100px" hidden>
             </div>
-            <!-- /.결과 출력 modal -->
 
             <!--  end panel-body -->
         </div>
@@ -35,3 +67,35 @@
 </div>
 <!-- /.row -->
 
+
+<%--
+    [테이블 생성용 Modal]
+    id : TableCreateModal
+--%>
+<div class="modal fade" id="TableCreateModal" tabindex="-1" role="dialog"
+     aria-labelledby="TableCreateModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"
+                        aria-hidden="true">&times;
+                </button>
+                <h4 class="modal-title" id="TableCreateModalLabel">형식 생성</h4>
+            </div>
+            <div class="modal-body">
+                <input type="hidden" id="createstatus" value="">
+                세로: <input type="number" id="col" value="0">
+                가로: <input type="number" id="row" value="0">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" id="createBtn">생성</button>
+                <button type="button" class="btn btn-default"
+                        data-dismiss="modal">닫기
+                </button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.결과 출력 modal -->
